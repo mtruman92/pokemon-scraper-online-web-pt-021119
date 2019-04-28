9b1d79d4c6f97beabe04409e59a846080ed3bb12
Mickae Truman <mtruman@nyit.edu>
2019-04-28 16:56:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,8 +22,10 @@ class Pokemon
   
   def self.find(id,db)
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?" , id).flatten
+    id = pokemon[0]
     name = pokemon[1]
     type = pokemon [2]
+    
   end
   
 end
