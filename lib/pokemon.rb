794fcc67f121d33498eb3d4f6d19b6c9bb76fa1c
Mickae Truman <mtruman@nyit.edu>
2019-04-28 16:43:32 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,7 @@ class Pokemon
     @@all 
   end
   
-  def self.save(id,name,type,db)
+  def self.save(name,type,db)
     db.execute("INSERT into pokemon (name, type) VALUES (?,?)",name,type)
   end
   
